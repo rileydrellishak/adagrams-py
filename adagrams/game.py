a5f31d4c468b07f729eea84ba2cd91c872b6ba81
Riley Drellishak <rileydrellishak@gmail.com>
2025-09-12 16:34:29 -0400

    Creates score chart as a constant dictionary to use in wave 3

--- adagrams/game.py
+++ adagrams/game.py
@@ -29,6 +29,35 @@ LETTER_POOL = {
     'Z': 1
 }
 
+SCORE_CHART = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+}
+
 def letter_pool_as_list():
     letter_pool_list = []
     for letter, frequency in LETTER_POOL.items():
