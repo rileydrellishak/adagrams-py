04df31367e5fd9e1ff4fee06280320b50655f5bd
Riley Drellishak <rileydrellishak@gmail.com>
2025-09-26 09:43:28 -0400

    Changes i to _ in letter_pool_as_list since variable was not used in later lines, scopes letter_pool dictionary, starting_letter_list, max_index_letter_list, and score_chart to respective functions"

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,25 +1,5 @@
 from random import randint
 
-LETTER_POOL = {
-    'A': 9, 'B': 2, 'C': 2, 'D': 4, 
-    'E': 12, 'F': 2, 'G': 3, 'H': 2, 
-    'I': 9, 'J': 1, 'K': 1, 'L': 4, 
-    'M': 2, 'N': 6, 'O': 8, 'P': 2, 
-    'Q': 1, 'R': 6, 'S': 4, 'T': 6, 
-    'U': 4, 'V': 2, 'W': 2, 
-    'X': 1, 'Y': 2, 'Z': 1
-}
-
-SCORE_CHART = {
-    'A': 1, 'B': 3, 'C': 3, 'D': 2, 
-    'E': 1, 'F': 4, 'G': 2, 'H': 4, 
-    'I': 1, 'J': 8, 'K': 5, 'L': 1, 
-    'M': 3, 'N': 1, 'O': 1, 'P': 3, 
-    'Q': 10, 'R': 1, 'S': 1, 'T': 1, 
-    'U': 1, 'V': 4, 'W': 4, 
-    'X': 8, 'Y': 4, 'Z': 10
-}
-
 def letter_pool_as_list():
     """Creates a list of the available letters.
 
@@ -29,15 +9,22 @@ def letter_pool_as_list():
     Returns:
         list: letter_pool_list (list of strings). Each string is a single capital letter. The number of each capital letter depends on fluency as defined in LETTER_POOL.
     """
+    LETTER_POOL = {
+    'A': 9, 'B': 2, 'C': 2, 'D': 4, 
+    'E': 12, 'F': 2, 'G': 3, 'H': 2, 
+    'I': 9, 'J': 1, 'K': 1, 'L': 4, 
+    'M': 2, 'N': 6, 'O': 8, 'P': 2, 
+    'Q': 1, 'R': 6, 'S': 4, 'T': 6, 
+    'U': 4, 'V': 2, 'W': 2, 
+    'X': 1, 'Y': 2, 'Z': 1
+    }
     letter_pool_list = []
     for letter, frequency in LETTER_POOL.items():
-        for i in range(0, frequency):
+        # Changed i to _ to signal that the loop runs x amount of times since variable i was not utilized
+        for _ in range(0, frequency): 
             letter_pool_list.append(letter)
 
     return letter_pool_list
-
-STARTING_LETTER_LIST = letter_pool_as_list()
-MAX_INDEX_LETTER_LIST = len(STARTING_LETTER_LIST) - 1
 
 def draw_letters():
     """Simulates drawing 10 tiles from a pile. Accounts for letter distribution.
@@ -48,12 +35,14 @@ def draw_letters():
     Returns:
         list: tiles (list of strings)
     """
+    STARTING_LETTER_LIST = letter_pool_as_list()
+    MAX_INDEX_LETTER_LIST = len(STARTING_LETTER_LIST) - 1
     tiles = []
     while len(tiles) < 10:
         random_index = randint(0, MAX_INDEX_LETTER_LIST)
         letter = STARTING_LETTER_LIST[random_index]
 
-        if tiles.count(letter) < LETTER_POOL[letter]:
+        if tiles.count(letter) < STARTING_LETTER_LIST.count(letter):
             tiles.append(letter)
         else:
             continue
@@ -88,6 +77,15 @@ def score_word(word):
     Returns
         int: score of word based on scoring guidelines for each letter.
     """
+    SCORE_CHART = {
+    'A': 1, 'B': 3, 'C': 3, 'D': 2, 
+    'E': 1, 'F': 4, 'G': 2, 'H': 4, 
+    'I': 1, 'J': 8, 'K': 5, 'L': 1, 
+    'M': 3, 'N': 1, 'O': 1, 'P': 3, 
+    'Q': 10, 'R': 1, 'S': 1, 'T': 1, 
+    'U': 1, 'V': 4, 'W': 4, 
+    'X': 8, 'Y': 4, 'Z': 10
+    }
     score = 0
     if len(word) >= 7 and len(word) <= 10:
         score += 8
